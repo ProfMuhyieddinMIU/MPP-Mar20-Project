7f0e114a23d206111ce3dc0c6bdb2be0c95a5f5b
Hussien Elgabry <hussien.elgabry@live.com>
2020-03-04 21:33:57 -0600

    refactor ui

--- src/ui/LoginWindow.java
+++ src/ui/LoginWindow.java
@@ -20,96 +20,113 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class LoginWindow extends Stage implements LibWindow {
+public class LoginWindow extends Stage implements LibWindow
+{
 	public static final LoginWindow INSTANCE = new LoginWindow();
-	
+
 	private boolean isInitialized = false;
-	
-	public boolean isInitialized() {
+
+	public boolean isInitialized()
+	{
 		return isInitialized;
 	}
-	public void isInitialized(boolean val) {
+
+	public void isInitialized(boolean val)
+	{
 		isInitialized = val;
 	}
+
 	private Text messageBar = new Text();
-	public void clear() {
+
+	public void clear()
+	{
 		messageBar.setText("");
 	}
-	
+
 	/* This class is a singleton */
-    private LoginWindow () {}
-    
-    public void init() { 
-        GridPane grid = new GridPane();
-        grid.setId("top-container");
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+	private LoginWindow()
+	{
+	}
 
-        Text scenetitle = new Text("Login");
-        scenetitle.setFont(Font.font("Harlow Solid Italic", FontWeight.NORMAL, 20)); //Tahoma
-        grid.add(scenetitle, 0, 0, 2, 1);
+	public void init()
+	{
+		GridPane grid = new GridPane();
+		grid.setId("top-container");
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
 
-        Label userName = new Label("User Name:");
-        grid.add(userName, 0, 1);
+		Text scenetitle = new Text("Login");
+		scenetitle.setFont(Font.font("Harlow Solid Italic", FontWeight.NORMAL, 20)); // Tahoma
+		grid.add(scenetitle, 0, 0, 2, 1);
 
-        TextField userTextField = new TextField();
-        //userTextField.setPrefColumnCount(10);
-        //userTextField.setPrefWidth(30);
-        grid.add(userTextField, 1, 1);
+		Label userName = new Label("User Name:");
+		grid.add(userName, 0, 1);
 
-        Label pw = new Label("Password:");
-        grid.add(pw, 0, 2);
-        grid.setGridLinesVisible(false) ;
+		TextField userTextField = new TextField();
+		// userTextField.setPrefColumnCount(10);
+		// userTextField.setPrefWidth(30);
+		grid.add(userTextField, 1, 1);
 
-        PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1, 2);
+		Label pw = new Label("Password:");
+		grid.add(pw, 0, 2);
+		grid.setGridLinesVisible(false);
 
-        Button loginBtn = new Button("Log in");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(loginBtn);
-        grid.add(hbBtn, 1, 4);
+		PasswordField pwBox = new PasswordField();
+		grid.add(pwBox, 1, 2);
 
-        HBox messageBox = new HBox(10);
-        messageBox.setAlignment(Pos.BOTTOM_RIGHT);
-        messageBox.getChildren().add(messageBar);;
-        grid.add(messageBox, 1, 6);
-        
-        loginBtn.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-        	public void handle(ActionEvent e) {
-        		try {
-        			ControllerInterface c = new SystemController();
-        			c.login(userTextField.getText().trim(), pwBox.getText().trim());
-        			messageBar.setFill(Start.Colors.green);
-             	    messageBar.setText("Login successful");
-        		} catch(LoginException ex) {
-        			messageBar.setFill(Start.Colors.red);
-        			messageBar.setText("Error! " + ex.getMessage());
-        		}
-        	   
-        	}
-        });
+		Button loginBtn = new Button("Log in");
+		HBox hbBtn = new HBox(10);
+		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+		hbBtn.getChildren().add(loginBtn);
+		grid.add(hbBtn, 1, 4);
 
-        Button backBtn = new Button("<= Back to Main");
-        backBtn.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-        	public void handle(ActionEvent e) {
-        		Start.hideAllWindows();
-        		Start.primStage().show();
-        	}
-        });
-        HBox hBack = new HBox(10);
-        hBack.setAlignment(Pos.BOTTOM_LEFT);
-        hBack.getChildren().add(backBtn);
-        grid.add(hBack, 0, 7);
-        Scene scene = new Scene(grid);
-        scene.getStylesheets().add(getClass().getResource("library.css").toExternalForm());
-        setScene(scene);
-        
-    }
-	
-	
+		HBox messageBox = new HBox(10);
+		messageBox.setAlignment(Pos.BOTTOM_RIGHT);
+		messageBox.getChildren().add(messageBar);
+		;
+		grid.add(messageBox, 1, 6);
+
+		loginBtn.setOnAction(new EventHandler<ActionEvent>()
+		{
+			@Override
+			public void handle(ActionEvent e)
+			{
+				try
+				{
+					ControllerInterface c = new SystemController();
+					c.login(userTextField.getText().trim(), pwBox.getText().trim());
+					messageBar.setFill(Start.Colors.green);
+					messageBar.setText("Login successful");
+				}
+				catch (LoginException ex)
+				{
+					messageBar.setFill(Start.Colors.red);
+					messageBar.setText("Error! " + ex.getMessage());
+				}
+
+			}
+		});
+
+		Button backBtn = new Button("<= Back to Main");
+		backBtn.setOnAction(new EventHandler<ActionEvent>()
+		{
+			@Override
+			public void handle(ActionEvent e)
+			{
+				Start.hideAllWindows();
+				Start.primStage().show();
+			}
+		});
+		HBox hBack = new HBox(10);
+		hBack.setAlignment(Pos.BOTTOM_LEFT);
+		hBack.getChildren().add(backBtn);
+		grid.add(hBack, 0, 7);
+		Scene scene = new Scene(grid);
+		scene.getStylesheets().add(getClass().getResource("library.css").toExternalForm());
+		setScene(scene);
+
+	}
+
 }

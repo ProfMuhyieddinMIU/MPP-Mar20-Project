c0607360eea31027b1d511e7cc7448162f07c480
Moustafa Bahnasawy <moustafa.a.galal@gmail.com>
2020-03-05 16:57:25 -0600

    Revert "Adding Admin Controller"
    This reverts commit 5de35bbf63f59767a0eee553382fd4ccb55a2d30.

--- src/business/controllers/impl/SystemController.java
+++ src/business/controllers/impl/SystemController.java
@@ -1,15 +1,18 @@
 package business.controllers.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import business.LoginException;
 import business.controllers.interfaces.ControllerInterface;
+import dataaccess.Auth;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessFacade;
 import dataaccess.User;
 
 public class SystemController implements ControllerInterface {
-	public static User currentLoggedInUser = null;
+	public static Auth currentAuth = null;
 
 	public void login(String id, String password) throws LoginException {
 		DataAccess da = new DataAccessFacade();
@@ -24,7 +27,22 @@ public class SystemController implements ControllerInterface {
 		String passwordFound = map.get(id).getPassword();
 		if (!passwordFound.equals(password))
 			throw new LoginException("Password incorrect");
-		currentLoggedInUser = map.get(id);
+		currentAuth = map.get(id).getAuthorization();
 	}
 
+	@Override
+	public List<String> allMemberIds() {
+		DataAccess da = new DataAccessFacade();
+		List<String> retval = new ArrayList<>();
+		retval.addAll(da.readMemberMap().keySet());
+		return retval;
+	}
+
+	@Override
+	public List<String> allBookIds() {
+		DataAccess da = new DataAccessFacade();
+		List<String> retval = new ArrayList<>();
+		retval.addAll(da.readBooksMap().keySet());
+		return retval;
+	}
 }
--- src/business/controllers/interfaces/ControllerInterface.java
+++ src/business/controllers/interfaces/ControllerInterface.java
@@ -1,10 +1,16 @@
 package business.controllers.interfaces;
 
+import java.util.List;
+
+import business.Book;
 import business.LoginException;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessFacade;
 
 public interface ControllerInterface
 {
 	public void login(String id, String password) throws LoginException;
-
+	public List<String> allMemberIds();
+	public List<String> allBookIds();
 	
 }

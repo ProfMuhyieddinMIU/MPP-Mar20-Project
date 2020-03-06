48f643489add8c4b98e889c075dc5b08b8c34390
611013 <611013@V47A-04-02.cs.mum.edu>
2020-03-06 11:30:18 -0600

    test

--- src/business/controllers/impl/Test11.java
+++ src/business/controllers/impl/Test11.java
@@ -1,13 +1,18 @@
 package business.controllers.impl;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.Period;
 
 public class Test11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 LocalDateTime currentDate = LocalDateTime.now();
-		    System.out.println("===== currentDate =>" + currentDate);
+		 LocalDateTime currentDate = LocalDateTime.now().minusDays(1);
+		// int days = Days.daysBetween(currentDate, LocalDateTime.now() ).getDays();
+		 Duration duration = Duration.between(LocalDateTime.now(), currentDate);
+		 long diff = Math.abs(duration.toDays() );
+		    System.out.println("===== currentDate =>" + diff);
 		    System.out.println("===== currentDate =>" + currentDate.plusDays(7) );
 	}
 

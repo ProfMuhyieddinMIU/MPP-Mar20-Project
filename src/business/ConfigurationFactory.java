7a05d98f7d159f65f576b0dd8973cccf6e86bb5f
Mostafa Mosad <mustafamosad57@gmail.com>
2020-03-06 12:19:37 -0600

    Adding Mail Sender Controller

---
+++ src/business/ConfigurationFactory.java
@@ -0,0 +1,21 @@
+package business;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigurationFactory {
+
+	public static String getProperty(String key) throws IOException {
+		InputStream input = new FileInputStream("src\\config.properties");
+
+		Properties prop = new Properties();
+
+		// load a properties file
+		prop.load(input);
+
+		return prop.getProperty(key);
+
+	}
+}

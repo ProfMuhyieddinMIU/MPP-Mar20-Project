ac715d61eea1e8c2f4df4a7e0b2a92135c9ef957
Hussien Elgabry <hussien.elgabry@live.com>
2020-03-05 22:24:31 -0600

    Removed not useful functions

--- src/business/Main.java
+++ src/business/Main.java
@@ -8,13 +8,13 @@ import dataaccess.DataAccessFacade;
 public class Main 
 {
 
+	/*Hus3/5/20:: NotUseful
 	public static void main(String[] args) 
 	{
 		System.out.println(allWhoseZipContains3());
 		System.out.println(allHavingOverdueBook());
 		System.out.println(allHavingMultipleAuthors());
 	}
-	
 	//Returns a list of all ids of LibraryMembers whose zipcode contains the digit 3
 	public static List<String> allWhoseZipContains3() 
 	{
@@ -25,8 +25,22 @@ public class Main
 		//implement
 		return null;
 	}
+	//Returns a list of all isbns of  Books that have multiple authors
+	public static List<String> allHavingMultipleAuthors() 
+	{
+		DataAccess da = new DataAccessFacade();
+		Collection<Book> books = da.readBooksMap().values();
+		List<Book> bs = new ArrayList<>();
+		bs.addAll(books);
+		//implement
+		return null;
+	}
+	*/
 	
-	//Returns a list of all ids of  LibraryMembers that have an overdue book
+	/**
+	 * 
+	 * @return A list of all ids of  LibraryMembers that have an overdue book
+	 */
 	public static List<String> allHavingOverdueBook() 
 	{
 		DataAccess da = new DataAccessFacade();
@@ -38,14 +52,5 @@ public class Main
 		
 	}
 	
-	//Returns a list of all isbns of  Books that have multiple authors
-	public static List<String> allHavingMultipleAuthors() 
-	{
-		DataAccess da = new DataAccessFacade();
-		Collection<Book> books = da.readBooksMap().values();
-		List<Book> bs = new ArrayList<>();
-		bs.addAll(books);
-		//implement
-		return null;
-	}
+	
 }
